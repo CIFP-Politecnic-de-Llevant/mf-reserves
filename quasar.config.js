40e2160e34f738c320a338a32db6401f2a5d1e2f
Joan Galmés Riera <joan.galmes@gmail.com>
2023-09-25 22:30:45 +0200

    bloqueig mentre es sincronitza

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '', //Original '/'
       // analyze: true,
       // env: {},
       // rawDefine: {}
@@ -196,7 +196,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'mf-reserves'
+        appId: 'reserves'
       }
     },
 
